d9794fb5b4fd37a0e7a5c898c22266e100136d4c
Kyle Buzsaki <kbuzsaki@ucsd.edu>
2014-12-28 15:56:23 -0800

    refactored client helper functions into a connection class

--- client.py
+++ client.py
@@ -1,20 +1,36 @@
 from urllib.request import urlopen
 import json
+import chess
+from chess import Board, Color, Position, Piece
 
-BASE_URL = "http://127.0.0.1:5000"
+LOCALHOST = "http://127.0.0.1:5000"
 
-def get(args):
-    url = BASE_URL + args
-    resp = json.loads(urlopen(url).read().decode('utf8'))
-    if "error" in resp:
-        print("ERROR: " + resp["error"])
-    return resp
+class GameConnection:
 
-def board():
-    pretty(get("/board"))
+    def __init__(self, base_url):
+        self.base_url = base_url
 
-def move(begin, end):
-    pretty(get("/move?begin=" + str(begin) + "&end=" + str(end)))
+    def _get(self, path, **kwargs):
+        url = self.base_url + path
+
+        # assemble url args
+        if kwargs:
+            url += "?" + "&".join([key + "=" + str(kwargs[key]) for key in kwargs])
+
+        resp = json.loads(urlopen(url).read().decode('utf8'))
+        return resp
+
+    def board(self):
+        return Board.from_notation(self._get("/board")["board"])
+
+    def turn(self):
+        return self._get("/turn")
+
+    def print_board(self):
+        pretty(self._get("/board"))
+
+    def move(self, begin, end):
+        self._get("/move", begin=begin, end=end)
 
 def pretty(resp):
     board = resp["board"]
