96d6819c79d7052f5b673d8348252213622c1041
Kyle Buzsaki <kbuzsaki@ucsd.edu>
2015-01-07 21:25:03 -0800

    added additional piece tests

--- tests/piece_test.py
+++ tests/piece_test.py
@@ -8,6 +8,14 @@ class PawnTest(unittest.TestCase):
     def setUp(self):
         self.board = Board.from_notation(chess.STARTING_NOTATION)
 
+    def test_row(self):
+        for piece in self.board.pieces():
+            self.assertEqual(piece.position.row, piece.row)
+
+    def test_col(self):
+        for piece in self.board.pieces():
+            self.assertEqual(piece.position.col, piece.col)
+
     def test_is_white(self):
         for piece in self.board.pieces():
             self.assertEqual(piece.color == Color.white, piece.is_white)
